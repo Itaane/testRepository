4ccf4bf3fd176b55a7b60fcfc7351ea2274eaf60
ShardsOfVoid <egtiger989@gmail.com>
2019-01-10 13:33:09 -0500

    Update gitProject.java

--- GitProject/src/gitProject.java
+++ GitProject/src/gitProject.java
@@ -6,18 +6,18 @@ public class gitProject {
 		System.out.println( "Type in your input in the format: \"x, y\"" );
 		Scanner scan = new Scanner( System.in );
 		String input = scan.nextLine();
-		
+		// fixed String spelling
 		System.out.println( input );
-		
+		// fixed System spelling
 		int loc = input.indexOf( ',' );
 		
 		System.out.println( loc );
 		
 		int x = Integer.parseInt( input.substring( 0, loc ) );
 		int y = Integer.parseInt( input.substring( loc + 2 ) );
-		
+		// fixed Parseint to parseInt
 		System.out.println( x + "\n" + y );
-		
+		// fixed System spelling
 		System.out.println( "Your randomly generated number between your defined bounds is: " + ( ( int )( Math.random() * ( y - x ) ) + x ) );
 		
 		scan.close();
